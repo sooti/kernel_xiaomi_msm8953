34f6ab2f0f0c7a8b0d667145e6485929b3a08dd9
sooti <sooti85@gmail.com>
2018-08-02 07:50:20 +0000

    Revert "Merge tag 'LA.UM.6.6.r1-09400-89xx.0' of https://source.codeaurora.org/quic/la/kernel/msm-3.18 into HEAD"
    This reverts commit ddbcb65e9da7c672bf8369d660120518c8154b4f.

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -1834,7 +1834,8 @@ static int fastrpc_file_free(struct fastrpc_file *fl)
 	spin_unlock(&fl->apps->hlock);
 
 	if (!fl->sctx) {
-		goto bail;
+		kfree(fl);
+		return 0;
 	}
 
 	(void)fastrpc_release_current_dsp_process(fl);
@@ -1846,9 +1847,6 @@ static int fastrpc_file_free(struct fastrpc_file *fl)
 	if (fl->ssrcount == fl->apps->channel[cid].ssrcount)
 		kref_put_mutex(&fl->apps->channel[cid].kref,
 				fastrpc_channel_close, &fl->apps->smd_mutex);
-
-bail:
-	mutex_destroy(&fl->map_mutex);
 	kfree(fl);
 	return 0;
 }
